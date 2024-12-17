fa020f44bae586773aec7f72aed9ef00801da9f3
Restless Being <57301218+PrinceDayani@users.noreply.github.com>
2024-12-17 05:42:09 +0530

    Update Home.jsx Fixed ig

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useEffect } from "react";
 import HeroSection from "../components/HeroSection";
 import PopularServiceHome from "../components/PopularServiceHome";
