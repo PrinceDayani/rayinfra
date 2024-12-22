a1767a2f7a8a07e92b8135de9ee24bf51c7f5666
Restless Being <57301218+PrinceDayani@users.noreply.github.com>
2024-12-22 12:55:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import ProjectPage from "./pages/ProjectPage";
+import Sector from "./pages/Sector";
+
+import Error404 from "./pages/Error404"; // Import your 404 page
 
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
+        {/* Define all your valid routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/Project" element={<ProjectPage />} />
+        <Route path="/Sector" element={<Sector />} /> 
+        {/* Catch-all route for undefined pages */}
+        <Route path="*" element={<Error404 />} />
       </Routes>
       <Footer />
     </Router>
