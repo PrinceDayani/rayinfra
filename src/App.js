05bf7e3cd295ad4f34805da4b8c564e1629cd48f
Restless Being <57301218+PrinceDayani@users.noreply.github.com>
2024-12-22 12:54:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
